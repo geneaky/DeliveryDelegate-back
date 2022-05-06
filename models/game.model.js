2f81c874926b209f0ac3f51345d4916c6ab3aad3
geneaky <kaktus418@gmail.com>
2022-05-06 17:40:15 +0900

    create game model

---
+++ models/game.model.js
@@ -0,0 +1,33 @@
+const Sequelize = require('sequelize');
+const Model = Sequelize.Model;
+const DataTypes = Sequelize.DataTypes;
+
+const Game = (sequelize) => sequelize.define('Game',{
+    game_id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    game_type: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    game_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    population: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    landmark_xpos: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    landmark_ypos: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+})
+
+module.exports = Game;
--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize');
 const {development} = require('../config/db.config');
 const User = require('./user.model');
 const Store = require('./store.model');
+const Game = require('./game.model');
 
 const db = {};
 const sequelize = new Sequelize(development.database, development.username, development.password, {host: development.host, dialect: development.dialect});
@@ -9,5 +10,6 @@ db.sequelize = sequelize;
 
 db.User = User(sequelize);
 db.Store = Store(sequelize);
+db.Game = Game(sequelize);
 
 module.exports = db;
