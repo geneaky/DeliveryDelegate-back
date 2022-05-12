36aeae82241599bd51a6a6d1f549120bfb076338
서유진 <youjin030700@gmail.com>
2022-05-12 20:21:18 +0900

    [feat] Review registered
    리뷰 등록 및 면제권 생성

---
+++ services/review_service.js
@@ -0,0 +1,46 @@
+const httpError = require('http-errors');
+const jwt = require('../api/middlewares/jwt');
+const {User} = require('../models');
+const {Review} = require('../models');
+const crypto = require('crypto');
+
+
+
+const writwReview = async (req, res, next) => {
+    /*if(사용자정보X && 가게정보X && 리뷰 내용 XX) {
+        return res.json({
+            message: 'Review registration failed'
+        });
+    }*/
+
+    await Review.create({
+        user_id : req.get(userid), // 사용자 정보
+        store_id :req.params.storeid,
+        content: req.body.review,
+    });
+
+
+
+    let counta = await Review.findAndCountAll({ // 리뷰 개수 조회
+        where: {
+            user_id : req.get(userid), // 사용자 정보
+        }
+    });
+
+    // 면제권 제공
+    if(counta % 10 == 0){
+        await User.increment('exemption_count', {by: 1}, //db에 추가할 것
+            {where : {
+                user_id : req.get(userid),
+            }}
+        );
+        res.status(200).json({message : 'Provide exemption '});
+    }
+   
+
+    res.status(200).json({message : 'Review registered'});
+};
+
+
+
+module.exports = writwReview;
