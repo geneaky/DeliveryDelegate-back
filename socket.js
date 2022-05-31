e67b1b970e5bd0ca9e0ec3835a6603b5a48352f9
geneaky <kaktus418@gmail.com>
2022-05-31 09:40:26 +0900

    game websocket

--- socket.js
+++ socket.js
@@ -11,10 +11,11 @@ const gameSocketNameSpace = io.of('/game');
 
 gameSocketNameSpace.on('connection', (socket) => {
 
+    //게임 참석
     socket.on('attend',async ({token,game_id,room_name, size}) => {
         const user = await jwt.verify(token);
 
-        if((await gameSocketNameSpace.in(room_name).allSockets()).size == size) {
+        if((await gameSocketNameSpace.in(room_name).allSockets()).size === size) {
             console.log('인원 초과');
             socket.emit('population', '인원 초과');
             return;
@@ -29,13 +30,14 @@ gameSocketNameSpace.on('connection', (socket) => {
         })
 
         socket.join(room_name);
-        socket.to(room_name).emit('hi', socket.id+'입장');
+        socket.to(room_name).emit('attend', socket.id+'입장');
     });
 
-    socket.on('game_end',async ({token,game_id,room_name,nickname,ranking}) => {
+    //안드로이드 게임 결과 전송
+    socket.on('game_result',async ({token,game_id,room_name,nickname,ranking}, next) => {
         const user = await jwt.verify(token);
 
-        Delegator.update({
+        await Delegator.update({
             ranking: ranking
         },{
             where:{
@@ -44,12 +46,96 @@ gameSocketNameSpace.on('connection', (socket) => {
             }
         }).catch((err) => {
             console.log(err);
-            return; // 재시도 처리 필요
+            return next(err); // 재시도 처리 필요
         });
 
-        socket.to(room_name).emit('end', '대표자'+ nickname);
+        if(ranking === 1) {
+            socket.to(room_name).emit('game_result', '대표자'+ nickname);
+        }
     });
 
+    //대표자 참석 확인 << 이건 안드로이드에서 일정 시간 내에 <확인> 버튼 안누르면 '대표자 탈주' 이벤트 호출
+    //대표자 어플 강제 종료시 << 안드로이드에서 어플 종료 이벤트 발생시 '대표자 탈주' 이벤트 호출하고
+
+    //대표자 탈주 -> 게임에서 탈주자 제거 -> 탈주 알림
+    socket.on('delegator_run_away', async(socket,{token,game_id,room_name,nickname,ranking}, next) => {
+        const user = await jwt.verify(token);
+        const user_id = user.id;
+
+        await Delegator.delete({
+            include:[{
+                model: User,
+                where: { user_id : user_id}
+            }],
+            where: User.user_id
+        }).catch((err) => {
+            console.log(err);
+            return next(err);
+        });
+
+        socket.broadcast.to(room_name).emit('delegator_run_away', '대표자가 탈주했습니다');
+    });
+
+    //대표자 다시 선정 -> 안드로이드에서 탈주 알림 받은 후 -> '대표자 다시 선정' 이벤트 호출
+    socket.on('delegator_re_ranking', async({token,game_id,room_name,nickname,ranking}, next) => {
+        const user = await jwt.verify(token);
+
+        const new_ranking = ranking - 1;
+
+        await Delegator.update({
+            ranking: new_ranking
+        },{
+            where:{
+                user_id: user.id,
+                game_id: game_id
+            }
+        }).catch((err) => {
+            console.log(err);
+            return next(err); // 재시도 처리 필요
+        });
+
+        if(new_ranking === 1) {
+            socket.to(room_name)
+                .emit('delegator_re_ranking', {
+                    msg:'대표자'+ nickname,
+                    ranking: new_ranking
+                });
+        }
+    });
+
+    //대표자 랜드마크 도착 -> 안드로이드에서 대표자가 랜드마크에 도착하면 해당 이벤트를 참여자들에게 알림
+    socket.on('delegator_arrive', ({token,game_id,room_name, }) => {
+        socket.broadcast.to(room_name).emit('delegator_arrive', '대표자가 랜드마크에 도착했습니다');
+    });
+
+    //게임 종료
+    socket.on('game_end',(socket) => {
+        socket.disconnect();
+    });
+
+    //게임 종료 후 게임 삭제 (대표자가 삭제)
+    socket.on('game_remove', async({room_name,ranking},next) => {
+        if(ranking === 1) {
+            await Delegator.delete({
+                include:[
+                    {
+                        model: Game,
+                        where: { socket_room_name : room_name }
+                    }],
+                where: { game_id : Game.game_id }
+            }).catch((err) => {
+                console.log(err);
+                return next(err);
+            });
+
+            await Game.delete({
+                where: { socket_room_name: room_name}
+            }).catch((err) => {
+                console.log(err);
+                return next(err);
+            })
+        }
+    })
 
 });
 
