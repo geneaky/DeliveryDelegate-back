e912e095b7692968465e14bb9070e72f0c6b054b
서유진 <youjin030700@gmail.com>
2022-06-06 16:32:21 +0900

    set multer

--- api/routes/review.js
+++ api/routes/review.js
@@ -2,23 +2,23 @@ const express = require('express');
 const reviewService = require('../../services/review_service');
 const router = express.Router();
 const multer = require('multer');
+const storage = multer.diskStorage({
+    destination:  (req, file, cb) => {
+      cb(null, 'uploads/')
+    },
+    filename:  (req, file, cb) => {
+      cb(null, "recieptTest."+file.mimetype.split('/')[1])// 파일 원본이름 저장
+    }
+  })
 
-const upload = multer({
-    storage: multer.diskStorage({
-      destination: function (req, file, cb) {
-        cb(null, '../../public');
-      },
-      filename: function (req, file, cb) {
-        cb(null, "recieptTest");
-      }
-    }),
-  });
+const upload = multer({ storage: storage }); 
 
 router.post('/post', async function(req, res, next) {
     reviewService.writwReview(req, res, next);
 });
 
 router.post('/reciept',  upload.single('file'), function(req, res, next) { //
+    console.log("multer됨");
     reviewService.recieptAuth(req, res, next);
 });
 
