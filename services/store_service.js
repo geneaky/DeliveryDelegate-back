6efe932a8ab9d589f0e4b436887cee6cfe7377c9
yujiniii <youjin030700@gmail.com>
2022-11-23 11:05:14 +0900

    fix: 좌표계변환부분 삭제
    클라이인트에서 바꿔주고 있었음 흐아,,,,,,

--- services/store_service.js
+++ services/store_service.js
@@ -1,56 +1,39 @@
 const httpError = require('http-errors');
-const axios = require('axios');
 const {Store, Review} = require('../models');
 const review = require('./review_service');
-require('dotenv').config();
 
-const kakaoMap = axios.create({
-    baseURL : "https://dapi.kakao.com/v2/local/geo",
-    headers : {
-        'Authorization' : `KakaoAK ${process.env.KAKAO_REST_API_KEY}`
-    }
-});
 
 
 const findStore = async (req, res, next) => {
-    await kakaoMap.get(`transcoord.json?x=${req.query.store_posx}&y=${req.query.store_posy}&input_coord=KTM&output_coord=WGS84`)
-    .then(async (result)=>{
-        const [geo] = result.data.documents
-
-        await Store.findOne({
-            where: {
-                store_name: req.query.store_name,
-                store_posx: geo.x,
-                store_posy: geo.y,
-            }
-        }).then((store) => {
-            if(store) {
-                return res.status(200).json({ store_id: store.store_id, message: "store existed"});
-            }
-            return res.status(404).json({ store_id: "null" ,message: "store not existed"});
-        }).catch((err) => {
-            next(httpError(500, err.message));
+    await Store.findOne({
+        where: {
+            store_name: req.query.store_name,
+            store_posx: req.query.store_posx,
+            store_posy: req.query.store_posy
+        }
+    }).then((store) => {
+        if(store) {
+            return res.status(200).json({ store_id: store.store_id, message: "store existed"});
+        }
+        return res.status(404).json({ store_id: "null" ,message: "store not existed"});
+    }).catch((err) => {
+        next(httpError(500, err.message));
         })
-    })
 }
 
 const registerStore = async (req, res, next) => {
-    await kakaoMap.get(`transcoord.json?x=${req.body.store_posx}&y=${req.body.store_posy}&input_coord=KTM&output_coord=WGS84`)
-        .then(async (result)=>{
-            const [geo] = result.data.documents   
-            
-            await Store.create({
-                store_name: req.body.store_name,
-                store_posx: geo.x,
-                store_posy: geo.y,
-                store_address: req.body.store_address
-            }).then((store) => {
-                return res.status(200).json({ store_id : store.store_id });
-            }).catch((err) => {
-                next(httpError(500,err.message));
-            });
-        })
-};
+    await Store.create({
+        store_name: req.body.store_name,
+        store_posx: req.body.store_posx,
+        store_posy: req.body.store_posy,
+        store_address: req.body.store_address
+    }).then((store) => {
+        return res.status(200).json({ store_id : store.store_id });
+    }).catch((err) => {
+        next(httpError(500,err.message));
+    });
+
+}
 
 const getReviews = async (req, res, next) => {
     console.log(req.params.id)
